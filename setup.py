47ba82b892e46573fab418cff284d765a21d2d11
Mohamed Abusaid <Mohamed Abusaid>
2016-08-18 21:07:41 +0100

    amended setup and added manifest

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Link
 * Source
   https://github.com/abusaidm/
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 version = "0.1.1"
 
@@ -17,12 +17,7 @@ setup(
     version=version,
     author="Mohamed Abusaid",
     author_email="m.abusaid<at>yahoo<dot>com",
-    packages=[
-        "sru",
-        "sru.conf",
-        "sru.support",
-        "sru.misc.service"
-        ],
+    packages=find_packages(),
     include_package_data=True,
     url="http://github.com/abusaidm/sru/packages/{}/".format(version),
 
@@ -32,9 +27,11 @@ setup(
     # Dependent packages (distributions)
     dependency_links=[
         "https://github.com/abusaidm/sru_pip/tarball/master#egg=sru_pip-0.1.0"
+        "https://github.com/abusaidm/sru_service/tarball/master#egg=sru_service-0.1.0"
     ],
     install_requires=[
         "aiohttp",
-        "sru_pip>=0.1.0"
+        "sru_pip>=0.1.0",
+        "sru_service>=0.1.0"
     ]
 )
