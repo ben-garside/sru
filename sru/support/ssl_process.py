8dcb07b793c1526e26d85a22a5ae941481cd9d31
teezdan <teezdan@gmail.com>
2016-08-22 08:36:12 +0100

    fixed bug with ssl context generation

--- sru/support/ssl_process.py
+++ sru/support/ssl_process.py
@@ -3,5 +3,5 @@ import ssl
 # SSL CONTEXT for HTTPS
 def ssl_context(ssl_cert, ssl_key):
     sslContext = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    sslContext.load_cert_chain(certFile, certKey)
+    sslContext.load_cert_chain(ssl_cert, ssl_key)
     return sslContext
